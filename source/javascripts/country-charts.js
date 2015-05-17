93628e7f96608989393e4066524179790d60c6d1
iandenty <denty29@gmail.com>
2015-05-17 16:54:34 +0100

    donut draft 1

--- source/javascripts/country-charts.js
+++ source/javascripts/country-charts.js
@@ -10,6 +10,8 @@ $(function () {
         columns: [
         ],
         types: {
+        },
+        colors: {
         }
       },
       axis: {
@@ -20,6 +22,39 @@ $(function () {
           }
         }
       }
+    }
+
+    var defaultDonut = {
+      bindto: '#chart2',
+      data: {
+        columns: [
+            ['default1', 100]
+        ],
+        type : 'donut',
+        colors: {
+          default1: '#e2e2e3'
+        }
+      }
+      ,
+      // legend: {
+      //   show: false
+      // },
+      donut: {
+          title: "",
+          // label: {
+          //   show: false
+          // }
+      }
+    //   ,
+    //   tooltip: {
+    //       show: false
+    //   }
+    }
+
+    var donutDetails = {
+      columns: [
+      ],
+      type: 'donut',
     }
 
     //Key for Cartodb account
@@ -56,6 +91,9 @@ $(function () {
           dataFields.push(data.rows[i].product_group);
         }
         for (var i = 0; i < dataFields.length; i++) {
+          hexColors = ['#1F77B4', '#FF7F0E', '#2CA02C', '#D62728'];
+          chartDetails.data.colors[dataFields[i]] = hexColors[i];
+          defaultDonut.data.colors[dataFields[i]] = hexColors[i];
           chartDetails.data.types[dataFields[i]] = chartType;
           getData(dataFields[i]);
         }
@@ -77,62 +115,64 @@ $(function () {
     getDataFields();
 
     setTimeout(function () {
-      var chart = c3.generate(chartDetails);
-      var rectangles = [].slice.call($('#chart1 svg').find('.c3-event-rect'));
+      var chart1 = c3.generate(chartDetails);
+      var chart2 = c3.generate(defaultDonut);
+      chart2.legend.hide('default1');
 
+      var rectangles = [].slice.call($('#chart1 svg').find('.c3-event-rect'));
       for (var i = 0; i < rectangles.length; i++) {
         appendRect(rectangles[i]);
       }
+
+      $('.overlay').on({
+        "mouseover": function() { 
+        },
+        "mouseout":  function() { 
+        }, 
+        "click":  function() { 
+
+          setTimeout(function () {
+            chart2.unload();
+          }, 500);
+
+          indexArray = $(this).attr("data-id");
+          populatePie(indexArray);
+
+          setTimeout(function () {
+            chart2.load(donutDetails);
+          }, 800);
+        }, 
+      });
+
     }, 1000);
 
-    // Chart2
-    var chart = c3.generate({
-        bindto: '#chart2',
-        data: {
-          columns: [
-              ['default1', 100]
-          ],
-          type : 'donut',
-          colors: {
-            default1: '#e2e2e3'
-          }
-        },
-        legend: {
-          show: false
-        },
-        donut: {
-            title: "",
-            label: {
-              show: false
-            }
-        },
-        tooltip: {
-            show: false
+
+    function appendRect(rectangle){
+      classNames = d3.select(rectangle).attr("class");
+      indexOfArrayNo = classNames.lastIndexOf("-");
+      arrayNo = classNames.substring(indexOfArrayNo+1, classNames.length);
+      width = rectangle.getBBox().width;
+      height = rectangle.getBBox().height;
+      $(rectangle).append('rect').attr({"class": "overlay", "data-id": arrayNo, "width": width , "height": height, "rx": "2", "ry": "2"});
+    }
+
+    function populatePie(indexArray){
+      donutDetails.columns.length = 0;
+      for (var i = 0; i < chartDetails.data.columns.length; i++) {
+        var dataColumn = [];
+        dataLabel = chartDetails.data.columns[i][0];
+        dataValue = chartDetails.data.columns[i][indexArray];
+        if(dataLabel !== year) {
+          dataColumn.push(dataLabel, dataValue);
+        } else {
+          defaultDonut.donut['title'] = dataValue;
         }
-    });
-
-  function appendRect(rectangle){
-    classNames = d3.select(rectangle).attr("class");
-    indexOfArrayNo = classNames.lastIndexOf("-");
-    arrayNo = classNames.substring(indexOfArrayNo+1, classNames.length);
-    width = rectangle.getBBox().width;
-    height = rectangle.getBBox().height;
-    $(rectangle).append('rect').attr({"class": "overlay", "data-id": arrayNo, "width": width , "height": height, "rx": "2", "ry": "2"})
-    .on({
-      "mouseover": function() { 
-
-       },
-      "mouseout":  function() { /* do stuff */ }, 
-      "click":  function() { 
-
-       }, 
-    });
-  }
-
-  function populatePie(){
-    
-  }
-
+        if(dataColumn.length > 0){
+          donutDetails.columns.push(dataColumn);
+        }
+        console.log(donutDetails.columns)
+      }
+    }
 
       
   }
