23b8ec89d051a8b94adb78c47dd020841215fb38
iandenty <denty29@gmail.com>
2015-07-22 21:07:26 +0100

    commodity data on load based on form values

--- source/javascripts/commodity.js
+++ source/javascripts/commodity.js
@@ -2,19 +2,7 @@ $(function() {
 
   if($('.commodity #map').length > 0){ 
 
-    var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png',{
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-    });
-
-    var map = L.map('map', {
-        scrollWheelZoom: false,
-        center: [0, 0],
-        zoom: 2
-    })
-
-  
-
-    map.addLayer(layer);
+    // ****form behaviour****
 
     //update output from slider
     $('#year').change(function(){
@@ -35,25 +23,32 @@ $(function() {
       }
     })
 
-    var sql = new cartodb.SQL({ user: 'iandenty', format: 'geojson'  });
+    // ****Map logic****
 
+    var map = L.map('map', {
+        scrollWheelZoom: false,
+        center: [0, 0],
+        zoom: 2
+    })
 
-    
-    // var  = L.geoJson().addTo(map);
-    var vpaLayer = {
-      "pre-negotiations" : L.geoJson(),
-      "negotiation" : L.geoJson(),
-      "preparation" : L.geoJson(),
-      "implementation" : L.geoJson(),
+    var layerSource = {
+      user_name: 'iandenty',
+      type: 'cartodb',
+      sublayers: []
     }
 
-    getVpa();
 
-    var countryLayerGroup = new L.LayerGroup();
+    var sublayers = [];
 
-    // L.control.layers(countryLayerGroup).addTo(map)
+
+    var baseLayer = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png',{
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    });
+
+    map.addLayer(baseLayer);
+
+
     var vpaCss = $("#vpa-css").text();
-    // countryLayerGroup.setCartoCSS();
 
     $('#commodity-form').submit(function(event){
       var values = $(this).serializeArray();
@@ -62,8 +57,8 @@ $(function() {
       var activeCountries = [];
 
       for(var i = 0; i < values.length; i++){
-        var name = values[i].name.toLowerCase()
-        var value = values[i].value.toLowerCase()
+        var name = values[i].name.toLowerCase();
+        var value = values[i].value;
         switch (name) {
           case "vpa-status":
             activeCountries.push(value);
@@ -77,48 +72,94 @@ $(function() {
         }
       }
       updateCountries(activeCountries);
-
     })
 
 
+    //add countries to layerSource
     function updateCountries(activeCountries){
-      countryLayerGroup.clearLayers();
+
       for(var i = 0; i < activeCountries.length; i++){
-
-        if(vpaLayer.hasOwnProperty(activeCountries[i])) {
-          countryLayerGroup.addLayer(vpaLayer[activeCountries[i]])
-        }
-
+        layerSource.sublayers.push({
+          sql: "SELECT * FROM country_iso_only_1 WHERE vpa_status='"+activeCountries[i]+"'",
+          cartocss: $('#vpa-css').text()
+        })
       }
-      countryLayerGroup.addTo(map)
+      loadMap(); 
+    }
+ 
+    // Add data layer to your map
+    function loadMap(){
+      cartodb.createLayer(map,layerSource)
+        .addTo(map)
+        .done(function(layer) {
+           // for (var i = 0; i < layer.getSubLayerCount(); i++) {
+           //     sublayers[i] = layer.getSubLayer(i);
+           // }
+        })
+        .error(function(err) {
+            console.log("error: " + err);
+        });
     }
 
 
-    function getVpa(){
-      var vpaCountryTable = 'country_iso_only_1';
-      var sqlStatement = "SELECT * " +
-                           "FROM "+vpaCountryTable+"";
-      sql.execute(sqlStatement)
-      .done(function(geojson) {
-        for(var i = 0; i < geojson.features.length; i++){
-          vpaStatus = geojson.features[i].properties.vpa_status.toLowerCase();
-          switch (vpaStatus) {
-            case "pre-negotiations":
-              vpaLayer["pre-negotiations"].addData(geojson.features[i]);
-              break;
-            case "negotiation":
-              vpaLayer["negotiation"].addData(geojson.features[i]);
-              break;
-            case "preparation":
-              vpaLayer["preparation"].addData(geojson.features[i]);
-              break;
-            case "implementation":
-              vpaLayer["implementation"].addData(geojson.features[i]);
-              break;
-          }
-        }
-      })
-    }
+    // var sql = new cartodb.SQL({ user: 'iandenty', format: 'geojson'  });
+
+
+    
+    // // var  = L.geoJson().addTo(map);
+    // var vpaLayer = {
+    //   "pre-negotiations" : L.geoJson(),
+    //   "negotiation" : L.geoJson(),
+    //   "preparation" : L.geoJson(),
+    //   "implementation" : L.geoJson(),
+    // }
+
+    // getVpa();
+
+    // var countryLayerGroup = new L.LayerGroup();
+
+    // L.control.layers(countryLayerGroup).addTo(map)
+
+
+
+    // function updateCountries(activeCountries){
+    //   countryLayerGroup.clearLayers();
+    //   for(var i = 0; i < activeCountries.length; i++){
+
+    //     if(vpaLayer.hasOwnProperty(activeCountries[i])) {
+    //       countryLayerGroup.addLayer(vpaLayer[activeCountries[i]])
+    //     }
+
+    //   }
+    //   countryLayerGroup.addTo(map)
+    // }
+
+
+    // function getVpa(){
+    //   var vpaCountryTable = 'country_iso_only_1';
+    //   var sqlStatement = "SELECT * " +
+    //                        "FROM "+vpaCountryTable+"";
+    //   sql.execute(sqlStatement)
+    //   .done(function(geojson) {
+    //     for(var i = 0; i < geojson.features.length; i++){
+    //       vpaStatus = geojson.features[i].properties.vpa_status.toLowerCase();
+    //       switch (vpaStatus) {
+    //         case "pre-negotiations":
+    //           vpaLayer["pre-negotiations"].addData(geojson.features[i]);
+    //           break;
+    //         case "negotiation":
+    //           vpaLayer["negotiation"].addData(geojson.features[i]);
+    //           break;
+    //         case "preparation":
+    //           vpaLayer["preparation"].addData(geojson.features[i]);
+    //           break;
+    //         case "implementation":
+    //           vpaLayer["implementation"].addData(geojson.features[i]);
+    //           break;
+    //       }
+    //     }
+    //   })
+    // }
 
 
 
